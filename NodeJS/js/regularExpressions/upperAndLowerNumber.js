626b143beb27d38ceccb5a6db9c29f032ad77f3b
OzerBey <yasinozeriletisim@gmail.com>
2022-02-11 21:06:42 +0300

    example completed

--- NodeJS/js/regularExpressions/upperAndLowerNumber.js
+++ NodeJS/js/regularExpressions/upperAndLowerNumber.js
@@ -22,3 +22,26 @@ let result = ohRegex.test(ohStr);
 let result2 = ohStr.match(ohRegex).length;
 console.log(result);
 console.log(result2);
+
+//examples
+let A5 = "haaaah";
+let A3 = "haah";
+let A100 = "h" + "a".repeat(100) + "h";
+let multipleB = /ha{3,}h/;
+multipleB.test(A5);
+multipleB.test(A3);
+multipleB.test(A100);
+
+//other example
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/; // Change this line
+let result6 = haRegex.test(haStr);
+
+console.log("a".repeat(5));
+
+//non matching
+let timStr = "Timmmmber";
+let timRegex = /Tim{4,29}ber/g; // Change this line
+let result = timRegex.test(timStr);
+let result2 = timStr.match(timRegex);
+console.log(result2);
