e6dac5f5c24ca937647de403e7622621501a2cc6
OzerBey <yasinozeriletisim@gmail.com>
2022-02-11 20:10:21 +0300

    regular Expressions (Regex) started to learn and apply

---
+++ NodeJS/js/regularExpressions/matchAllNumbers.js
@@ -0,0 +1,9 @@
+/**Match All Numbers
+
+The shortcut to look for digit characters is \d, with a lowercase d. This is equal to the character class [0-9], which looks for a single character of any number between zero and nine. 
+\d is the same as [0-9].
+*/
+
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g; // Change this line
+let result = movieName.match(numRegex).length;
