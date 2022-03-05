9decbedf77cc3ee770b3c09136a6ef7e06f52837
OzerBey <yasinozeriletisim@gmail.com>
2022-03-05 03:08:44 +0300

    useEffect using started and implementations

--- React/react-project3/react-app/src/App.js
+++ React/react-project3/react-app/src/App.js
@@ -1,22 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [name, setName] = useState("Yasin"); //Yasin is a default value for name useState
+  const [age, setAge] = useState(24); //20 is a default value for age useState
+  useEffect(() => {
+    console.log("The state updated");
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>
+        {name} is {age}
+        <button
+          style={{
+            marginLeft: "10px",
+            color: "white",
+            backgroundColor: "black",
+          }}
+          onClick={() => {
+            setAge(age + 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {" "}
+          Click
+        </button>
+      </h1>
     </div>
   );
 }
